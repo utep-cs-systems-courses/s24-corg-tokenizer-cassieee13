3f18299762ad1008bbab6809dda4018ee414dfa6
Cassandra Alvarez <cealvarez2@miners.utep.edu>
2024-02-15 21:12:27 -0700

    Created the simple UI

---
+++ src/token.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define MAX_INPUT_LEN 100
+int main( )
+{
+  char input[MAX_INPUT_LEN];
+  
+  printf("The program name is Tokenizer. Please input a message:\n> ");
+  fgets(input, MAX_INPUT_LEN, stdin);
+  printf("Your input was: %s", input);
+}
