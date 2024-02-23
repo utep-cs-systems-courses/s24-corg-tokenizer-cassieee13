cda4edb2faa023de4d9fc1d8f4a328a4e85a562a
Cassandra Alvarez <cealvarez2@miners.utep.edu>
2024-02-22 23:07:21 -0700

    completed token_terminator and token_start methods

--- src/token.c
+++ src/token.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "tokenizer.c"
 
 #define MAX_INPUT_LEN 100
 int main( )
@@ -8,8 +9,9 @@ int main( )
   printf("The program name is Tokenizer. Please input a message:\n> ");
   // fgets(input, MAX_INPUT_LEN, stdin);
   //printf("Your input was: %s", input);
-  char str[] = "Hello ";
+  char str[] = "Hello World";
   char* point = str;
   printf("the value inside input: %c\n", point[0]);
-  
+
+  printf("0 for no, 1 for yes: %d\n", token_terminator( point ) );
 }
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -18,13 +18,25 @@ int non_space_char(char c)
   return 0;
 }
 
-char *token_start(char *str)
+char *token_start(char* str)
 {
-  int index = 1;
-  while( *str[index] != '\0' ){ //all strings aka char arrays end with the '\0'
+  int index = 0;
+  while( str[index] != '\0' ){ //all strings aka char arrays end with the '\0'
     
-   if( space_char(*str[index]) ){//if you find a space in the char[]
-     return *str[index+1];//return the next value which is the start of the next token
+   if( space_char( str[index]) ){//if you find a space in the char[]
+     return str[index+1];//return the next value which is the start of the next token
+   }
+   index++;
+  }
+  return 0;
+}
+
+char *token_terminator(char *token)
+{
+  int index = 0;
+  while( token[index] != '\0' ){
+   if( space_char( token[index] )){
+     return token[index-1]; //when it finds the space, that means the token has ended.
    }
    index++;
   }
